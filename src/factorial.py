211fb5f7b9197b7698e77269fe1bd6ca7a308b76
Kike Marchuet <kike@marchuet.es>
2025-02-12 23:40:59 +0100

    Crear directores src i tests

---
+++ src/factorial.py
@@ -0,0 +1,19 @@
+def factorial(num):
+
+    # Si no és un entero, té que donar error
+    if not isinstance(num, int):
+        raise TypeError("El número deu ser un entero")
+    # Si és un booleà, té que donar error
+    if isinstance(num, bool):
+        raise TypeError("El número no pot ser un booleà")
+
+    # Error si és negatiu
+    if (num < 0):
+        raise ValueError("El número deu ser major que zero")
+
+    # Situació base
+    if num == 0 or num == 1:
+        return 1
+
+    # Recursivitat
+    return num * factorial(num - 1)  # Recursión
---
+++ tests/test_factorial.py
@@ -0,0 +1,26 @@
+import src.factorial as factorial
+import pytest
+
+def test_factorial_1():
+    assert factorial.factorial(1) == 1
+
+def test_no_entero():
+
+    # Si passem una cadena
+    with pytest.raises(TypeError):
+        factorial.factorial("a")
+
+    # Si passem un float
+    with pytest.raises(TypeError):
+        factorial.factorial(3.5)
+
+    # Si passem un booleà
+    with pytest.raises(TypeError):
+        factorial.factorial(True)
+
+def test_negativo():
+    with pytest.raises(ValueError):
+        factorial.factorial(-5)
+
+def test_5():
+    assert factorial.factorial(5) == 120
